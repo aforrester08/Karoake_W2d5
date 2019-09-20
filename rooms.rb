01bb622a195ba698f4d2bda9ce7a1f8a678446c8
Alex Forrester <alexforrester96@gmail.com>
2019-09-20 11:49:21 +0100

    finsihed extension

--- rooms.rb
+++ rooms.rb
@@ -1,18 +1,29 @@
 class Room
 
   attr_reader :room_number, :occupants, :songs
+  attr_accessor :till
 
   def initialize(room_number)
     @room_number = room_number
     @occupants = []
     @songs = []
+    @max_capacity = 3
+    @entry_fee = 3
   end
 
   def check_in_guest(guest)
-    @occupants.push(guest)
+    if @occupants.length() < @max_capacity
+      @occupants.push(guest)
+      guest.wallet -= @entry_fee
+    end
   end
 
   def check_out_guest(guest)
     @occupants.delete(guest)
   end
+
+  def add_song_to_room(song)
+    @songs.push(song)
+  end
+
 end
--- specs/rooms_specs.rb
+++ specs/rooms_specs.rb
@@ -8,8 +8,11 @@ class TestRoom < Minitest::Test
 
   def setup
     @room1 = Room.new(1)
-    @guest1 = Guest.new("Alex")
-    @guest2 = Guest.new("David")
+    @guest1 = Guest.new("Alex", 20)
+    @guest2 = Guest.new("David", 20)
+    @guest3 = Guest.new("Oli", 20)
+    @guest4 = Guest.new("Mikey", 20)
+    @song1 = Song.new("Mr Brightside")
   end
 
   def test_get_room_number()
@@ -28,5 +31,21 @@ class TestRoom < Minitest::Test
     assert_equal([], @room1.occupants)
   end
 
-  
+  def test_add_song_to_room()
+    @room1.add_song_to_room(@song1)
+    assert_equal([@song1], @room1.songs)
+  end
+
+  def test_max_room_capacity()
+    @room1.check_in_guest(@guest1)
+    @room1.check_in_guest(@guest2)
+    @room1.check_in_guest(@guest3)
+    @room1.check_in_guest(@guest4)
+    assert_equal([@guest1, @guest2, @guest3], @room1.occupants)
+  end
+
+  def test_entry_fee()
+    @room1.check_in_guest(@guest1)
+    assert_equal(17, @guest1.wallet)
+  end
 end
